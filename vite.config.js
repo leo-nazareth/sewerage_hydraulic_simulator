804fe83f79a10b564c1dbb3c2f6c54e54931a535
Leonardo Nazareth <leonazareth@gmail.com>
2025-11-18 19:36:53 -0400

    config(deploy): configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/sewerage_hydraulic_simulator/',
   plugins: [react()],
   resolve: {
     alias: {
