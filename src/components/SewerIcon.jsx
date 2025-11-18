a4de3908ff120401db5f01e3e4a915d5d86e5796
Leonardo Nazareth <leonazareth@gmail.com>
2025-11-18 07:21:38 -0400

    feat(ui): use actual Vecteezy sewer icon image instead of custom SVG

---
+++ src/components/SewerIcon.jsx
@@ -0,0 +1,11 @@
+const SewerIcon = ({ className = "w-10 h-10" }) => {
+  return (
+    <img 
+      src="/Icons/vecteezy_sewer-vector-icon_17217062.jpg"
+      alt="Sewer icon"
+      className={className}
+    />
+  );
+};
+
+export default SewerIcon;
